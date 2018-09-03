d536d9ad5010357f5cbfa92891da37c49b6e7341
Kendrick Taylor <kendrick@circuitsix.com>
2018-09-03 14:03:52 -0700

    update location of config files

--- conf.js
+++ conf.js
@@ -6,9 +6,9 @@ const os = require('os');
 module.exports = {
   SCOPES: ['https://www.googleapis.com/auth/calendar'],
 
-  CRED_PATH: path.join(os.homedir(), 'client_secret.json'),
+  CRED_PATH: path.join(os.homedir(), '.gcal/client_secret.json'),
 
-  TOKEN_PATH: path.join(os.homedir(), 'calendar_api_token.json'),
+  TOKEN_PATH: path.join(os.homedir(), '.gcal/calendar_api_token.json'),
 
   CALENDAR_ID: 'primary',
 
